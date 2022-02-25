db459d355b9c2ea41ae1f4a8c9f54ce8250b2721
Даниил <@WIN-ENF4NR8ET31>
2022-02-25 21:34:20 +0300

    fix

--- Krestiki2D/Krestiki2D.py
+++ Krestiki2D/Krestiki2D.py
@@ -2,30 +2,48 @@
 print ("Возврат хода - буква R")
 setka=int(input('введи размер желаемого поля: '))
 fulldlina=len(str(setka**2))
-
+ 
 def is_integer(s):
     try:
         int(s)
         return True
     except ValueError:
         return False 
-     
+massive=[] 
+current2=1
 def vvod_XO(bukva_,n):
     x=input('следующий ход игрока' + ' '+ bukva_ +', выберите номер: ')
-
+    
     if x.upper()=='R' :
         if  len(arraysteps)!=0:
             stepp=arraysteps[len(arraysteps)-1]# последний ход
-            arraysetka[stepp-1]=stepp
+            current2=1
+            for i in range(setka):
+                mass=[]
+                for k in range(setka):
+                    mass.append(current2)
+                    if massive[i][k]==stepp:
+                        arraysetka[i][k]=stepp
+                        break
+                    current2+=1
+                massive.append(mass)
             arraysteps.pop(len(arraysteps)-1)
+     # готово
     if is_integer(x) :
         x=int(x)
         try:
             if not(x<=n**2 and x>0 ):
                 raise BaseException("введи допустимое значение")
-            if not(x in arraysetka):
-                raise BaseException("поле занято")
-            arraysetka[x-1]=bukva_
+            
+            if x in arraysteps:
+                raise BaseException("поле занято")            
+            current=1
+            for i in range(setka):
+                for k in range(setka):
+                    if x==current:
+                        arraysetka[i][k]=bukva_
+                    current+=1
+                      
             arraysteps.append(x)
         except BaseException as ve:
           print(ve)
@@ -36,16 +54,6 @@ def ResearchOfSize(current_element):
     global fulldlina
     dlina=len(str(current_element))
     return fulldlina-dlina
-
-
-def vuvod_stroka(setka_,k): 
-    stroka='|'
-    maxlensymbol=len(str(setka_**2))
-    for i in range(k,k+setka_):
-        stroka+=str(arraysetka[i])
-        stroka+=' '*ResearchOfSize(arraysetka[i])
-        stroka+='|'
-    return stroka
 
 def ToConsoleArray(setka_):    
     t=0
@@ -72,14 +80,15 @@ def win(setka_):
     ADiagDown=[]
     ADiagUp=[]
     for i in range(setka_):
+       Astolbec.clear()
+       Astroka.clear()
        for j in range(setka_):
              Astolbec.append(arraysetka[j][i])
              Astroka.append(arraysetka[i][j])
-       ADiagUp.append(arraysetka[i][setka_-i])      
+       ADiagUp.append(arraysetka[i][j-i])      
        ADiagDown.append(arraysetka[i][i])
-
        if askwin(Astolbec) or askwin(Astroka): return True
-       if askwin(ADiagDown) or askwin(ADiagUp): return True
+    if askwin(ADiagDown) or askwin(ADiagUp): return True
     return False
 
 def GetSymbol():    
